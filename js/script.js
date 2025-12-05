93c90cc18c38b71bb76992a4ca9ebf505e56b95d
isabelleLj <isabelle.ljunggren@outlook.com>
2025-12-05 12:57:15 +0100

    Updated 25-12-05 12:56

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 
+
+
+
 let numberOfRounds = 0;
 let score = 0;
 let totalScore = 0;
-
-
-
 
 const main = document.querySelector('main');
 
@@ -21,26 +21,29 @@ const btnHold = document.querySelector('#hold');
 const btnNewGame = document.querySelector('New Game')
 
 
-function newName(){
-    const addName = prompt(' Enter your name:');
-    playerName.innerText = `Player:${addName}`;
 
 
-};
 
 
 function theDiceRoll (event){
     event.preventDefault();
     let dice = Math.floor(Math.random()*6)+1;
     score += dice;
+
     roundScore.innerText = `Your score on this round is : ${score}`;
-    console.log(dice)
     diceResult.innerText = `The dice landed on ${dice}`;
+
     if(dice == 1){
         score = 0;
+        resetRound();
         roundScore.innerText = `OH NO you rolled a ${dice}.\n Your round score have been reset!\n Roll the dice again.`;
     } 
     
+}
+
+function resetRound () {
+    numberOfRounds++;
+    yourRounds.innerText = `Number of rounds: ${numberOfRounds}`;
 }
 
 function hold(event){
@@ -48,20 +51,24 @@ function hold(event){
     totalScore += score;
     endOfGame();
     score = 0;
+    resetRound();
+
+
     totalPoints.innerText = `Total points: ${totalScore}`;
-    numberOfRounds++;
     yourRounds.innerText = `Number of rounds: ${numberOfRounds}`;
 
 }
 
 function endOfGame(){
     if (totalScore >= 100){
-    alert(`Grattis! Du vann spelet!\nTotal poäng: ${totalScore}\nAntal omgångar: ${numberOfRounds}`);
+    alert(`congratulations, you won the game!\nTotal points: ${totalScore}\nAntal omgångar: ${numberOfRounds}`);
     }
 }
 
 
-playerName.addEventListener('click', newName);
+
+
+
 btnRoll.addEventListener('click', theDiceRoll);
 btnHold.addEventListener('click', hold);
 
